2064d4a7d20caa05bdd87ec31e4e06eb552ae80e
Alexander <45214837+Dualve@users.noreply.github.com>
2019-09-26 19:48:04 +0300

    Add files via upload

---
+++ Unit1.pas
@@ -0,0 +1,63 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Label1: TLabel;
+    Memo1: TMemo;
+    RadioGroup1: TRadioGroup;
+    Button1: TButton;
+    CheckBox1: TCheckBox;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+
+begin
+  Memo1.Clear;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+
+var add: string[4];
+    year: string[3];
+
+begin
+  if CheckBox1.checked then
+  begin
+    Memo1.Clear;
+    Memo1.Lines.Add('1)Введите ваш возраст в поле возраст.');
+    Memo1.Lines.Add('2)Выберите подходящий вариант в блоке справа.');
+    Memo1.Lines.Add('3)Отключите переключатель "Информация".');
+    Memo1.Lines.Add('4)Нажмите "Написать".');
+  end
+  else
+    begin
+    Memo1.Clear;
+    year := Edit1.Text;
+    Memo1.Lines.Add('Вы прожили ' + year + ' ' + RadioGroup1.Items[RadioGroup1.ItemIndex] + '.');
+    end;
+
+
+end;
+
+end.
+
